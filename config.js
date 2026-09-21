2a21b7ed1ea80a35cfc0e0abddc079ab5dc4c2ad
Manoj-bisht0806 <manojbisht.0806@gmail.com>
2026-09-21 10:54:02 +0530

    Add configuration for plants, statuses, and priorities

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    plants: ["P538", "P452", "P546"],
+    statuses: ["Open", "Closed", "Rejected"],
+    priorities: ["High", "Medium", "Low"],
+    localImportOptions: ["Local", "Import"]
+};
