5fce1a813387a3f2bd0b4a23b624ef6e82f0b097
Manoj-bisht0806 <manojbisht.0806@gmail.com>
2026-09-21 11:05:23 +0530

    Remove priorities from CONFIG
    Removed priorities from the configuration.

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
     plants: ["P538", "P452", "P546"],
     statuses: ["Open", "Closed", "Rejected"],
-    priorities: ["High", "Medium", "Low"],
     localImportOptions: ["Local", "Import"]
 };
