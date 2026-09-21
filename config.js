550af05c2a73abbe2ac50dd742aad36bed00287f
Manoj-bisht0806 <manojbisht.0806@gmail.com>
2026-09-21 11:22:53 +0530

    Add priorities to configuration

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
     plants: ["P538", "P452", "P546"],
     statuses: ["Open", "Closed", "Rejected"],
+    priorities: ["A", "B", "C", "D"],
     localImportOptions: ["Local", "Import"]
 };
